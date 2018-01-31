241b2d49ea20d545490a829b308889c9694f278a
halprin <halprin@users.noreply.github.com>
2018-01-30 17:01:47 -0700

    Decrypt previously encrypted files

--- backup/encryptedfile.py
+++ backup/encryptedfile.py
@@ -26,5 +26,30 @@ class EncryptedFile:
                 for chunk in encryptor:
                     cipher_file.write(chunk)
 
+    def decrypt(self):
+        kms_key_provider = KMSMasterKeyProvider(key_ids=[self.kms_key],
+                                                botocore_session=Session(profile=self.aws_profile))
+
+        with open(self.out_path, 'rb') as cipher_file, open(self.path, 'wb') as plain_file:
+            with aws_encryption_sdk.stream(mode='d', source=cipher_file, key_provider=kms_key_provider) as decryptor:
+                encryption_context = decryptor.header.encryption_context
+                if self._dictionary_is_subset(encryption_context, self.ENCRYPTION_CONTEXT) is False:
+                    print("Decrypted context doesn't match original context! {}".format(encryption_context))
+                    return
+                for chunk in decryptor:
+                    plain_file.write(chunk)
+
     def encrypted_path(self):
         return self.out_path
+
+    @staticmethod
+    def _dictionary_is_subset(superset, subset):
+        is_subset = True
+        for item in subset.items():
+            key = item[0]
+            if key not in superset or superset[key] != item[1]:
+                is_subset = False
+                break
+
+        return is_subset
+
